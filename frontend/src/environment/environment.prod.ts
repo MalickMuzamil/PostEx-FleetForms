665fdadd6ede1a61e47da4c294d49497550c0bba
Hafiz Muzamil <130305212+MalickMuzamil@users.noreply.github.com>
2026-02-23 10:16:51 +0500

    Change apiBaseUrl to relative path in production config

--- frontend/src/environment/environment.prod.ts
+++ frontend/src/environment/environment.prod.ts
@@ -1,9 +1,10 @@
 export const environment = {
   production: true,
 
-  apiBaseUrl: 'http://localhost:5000',
+  apiBaseUrl: '/api',
 
   POSTEX_BASE_URL: 'https://auth-stage.postexglobal.com',
   POSTEX_PUBLIC_API_KEY: 'pk_live_2rrww4kx7Gew27kxNM0EP8ZGC0QPGZNC',
   POSTEX_SECRET_API_KEY: 'sk_live_KCeW85iVQktuqlQMSr5SWo5zMoP5qznc'
+
 };
