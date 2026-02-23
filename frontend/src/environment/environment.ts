f5ad816e6cb3c996c607e5608180f109ac5adb3c
Hafiz Muzamil <130305212+MalickMuzamil@users.noreply.github.com>
2026-02-23 10:16:08 +0500

    Add files via upload

---
+++ frontend/src/environment/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+
+  apiBaseUrl: 'http://localhost:5000',
+
+  POSTEX_BASE_URL: 'https://auth-stage.postexglobal.com',
+  POSTEX_PUBLIC_API_KEY: 'pk_live_2rrww4kx7Gew27kxNM0EP8ZGC0QPGZNC',
+  POSTEX_SECRET_API_KEY: 'sk_live_KCeW85iVQktuqlQMSr5SWo5zMoP5qznc'
+};
