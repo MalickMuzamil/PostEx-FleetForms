0c06f4128e31b4ce5c1d0ce1ef14ab5ea878c774
muzamilsaleem-PostEx <muzamil.saleem@postexglobal.com>
2026-04-09 19:53:11 +0500

    Credentials Updated

--- frontend/src/environment/environment.prod.ts
+++ frontend/src/environment/environment.prod.ts
@@ -7,15 +7,15 @@ export const environment = {
    // =========================
   // STAGE / LOCAL
   // // =========================
-  POSTEX_BASE_URL: 'https://auth-stage.postexglobal.com',
-  POSTEX_PUBLIC_API_KEY: 'pk_live_5PWC7_uqHQeRFcHmVkTDyGqyUS1b-BhA',
-  POSTEX_SECRET_API_KEY: 'sk_live_wse6tjvrX-Tgtw5juSwal78bGy9PgwUd'
+  // POSTEX_BASE_URL: 'https://auth-stage.postexglobal.com',
+  // POSTEX_PUBLIC_API_KEY: 'pk_live_5PWC7_uqHQeRFcHmVkTDyGqyUS1b-BhA',
+  // POSTEX_SECRET_API_KEY: 'sk_live_wse6tjvrX-Tgtw5juSwal78bGy9PgwUd'
 
 
    // =========================
   // PRODUCTION
   // =========================
-  // POSTEX_BASE_URL: 'https://auth.postexglobal.com',
-  // POSTEX_PUBLIC_API_KEY: 'pk_live_5PWC7_uqHQeRFcHmVkTDyGqyUS1b-BhA',
-  // POSTEX_SECRET_API_KEY: 'sk_live_wse6tjvrX-Tgtw5juSwal78bGy9PgwUd',
+  POSTEX_BASE_URL: 'https://auth.postexglobal.com',
+  POSTEX_PUBLIC_API_KEY: 'pk_live_5PWC7_uqHQeRFcHmVkTDyGqyUS1b-BhA',
+  POSTEX_SECRET_API_KEY: 'sk_live_wse6tjvrX-Tgtw5juSwal78bGy9PgwUd',
 };
